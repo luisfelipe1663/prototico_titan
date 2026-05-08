10d84f23e94d35ce0f56ccb072e2b080a73f841f
ciroa19 <51929419+ciroa19@users.noreply.github.com>
2026-05-07 21:14:59 -0300

    finalizando home, criação logim

--- interacao.js
+++ interacao.js
@@ -1,38 +1,40 @@
 function toggleMenu() {
-        document.getElementById("menu").classList.toggle("active");
-    }
+    document.getElementById("menu").classList.toggle("active");
+}
 
-    const sobreNos = document.querySelector(".sobrenos-home");
+/* ANIMAÇÃO SOBRE NÓS */
+const sobreNos = document.querySelector(".sobrenos-home");
 
-    const observerSobre = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                sobreNos.classList.add("animar");
-            }
-        });
-    }, {
-        threshold: 0.3
+const observerSobre = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            sobreNos.classList.add("animar");
+            observerSobre.disconnect();
+        }
     });
+}, {
+    threshold: 0.3
+});
 
-    if (sobreNos) {
-        observerSobre.observe(sobreNos);
-    }
-    const planos = document.querySelectorAll(
+if (sobreNos) {
+    observerSobre.observe(sobreNos);
+}
+
+/* ANIMAÇÃO PLANOS */
+const planos = document.querySelectorAll(
     ".plano_basico, .plano_avancado, .plano_premium"
 );
 
 const observerPlanos = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
-
-            // anima um por vez
             planos.forEach((plano, index) => {
                 setTimeout(() => {
                     plano.classList.add("animar");
                 }, index * 300);
             });
 
-            observerPlanos.disconnect(); // roda só uma vez
+            observerPlanos.disconnect();
         }
     });
 }, {
@@ -44,3 +46,26 @@ const secaoPlanos = document.querySelector(".caixa-planos_home");
 if (secaoPlanos) {
     observerPlanos.observe(secaoPlanos);
 }
+
+/* ANIMAÇÃO SERVIÇOS */
+const servicos = document.querySelectorAll(".animar-servico");
+
+const observerServicos = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            servicos.forEach((servico, index) => {
+                setTimeout(() => {
+                    servico.classList.add("ativo");
+                }, index * 250);
+            });
+
+            observerServicos.disconnect();
+        }
+    });
+}, {
+    threshold: 0.2
+});
+
+servicos.forEach((servico) => {
+    observerServicos.observe(servico);
+});
